7a8e328e40bfde021a175c148b0599c86a8fa8aa
Marc Rijken <marc@rijken.org>
2024-11-03 20:24:39 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Merge Settings README
+
+"merge-settings" let you read settings from one or more sources and merge these settings
+with the your workspace / folder settings.
+
+## Features
+
+Merge Settings will be executed when you execute the command `Merge Settings`  via the command palette.
+
+It will read settings from the next sources:
+- workspace folders if `mergeSettings.useWorkspaceFolders` is set to true (default). If you
+  are in a workspace, all folders of the workspace are iterated (in the order as specified in
+  the workspace file) over. Every workspace folder is checked for a file `.vscode/settings.default.json`.
+  The file needs to be a VSCode settings.json compliant file.
+  (or a different name specifed in `mergeSettings.workspaceFolderSettingsFile`). All
+- a list of files as `mergeSettings.sourceSettingsFiles`. A file can be a localfile or an URL to settings.json
+  compliant file.
+
+The read settings will, be merged into the workspace settings. That will be the settings object
+of the `.workspace` (when you are in a multiroot workspace) or `.vscode/settings.json` in the current folder otherwise.
+
+## Extension Settings
+
+This extension contributes the following settings:
+
+- `mergeSettings.useWorkspaceFolders`: Check the workspacefolders (defaults to `true`)
+- `mergeSettings.workspaceFolderSettingsFile`: The file in the workspacefolders which contain the settings to merge
+  (defaults to `.vscode/default.settings.json`).
+- `mergeSettings.sourceSettingsFiles`: A list of files with the settings to merge.
+
+## Release Notes
+
+### 0.1.0
+
+Initial release
---
+++ src/extension.ts
@@ -0,0 +1,150 @@
+import * as fs from "fs";
+import * as path from "path";
+import { promisify } from "util";
+
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+
+import { parse, ParseError } from "jsonc-parser";
+
+const exists = promisify(fs.exists);
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+
+const COMMAND_NAME = "MergeSettings.sync";
+
+async function mergeSettings(sourceSettingsContent: string, destinationFile: string) {
+	let log = vscode.window.createOutputChannel("MergeSettings");
+
+	// get source settings
+	const sourceSettingsErrors: ParseError[] = [];
+	const sourceSettings = parse(
+		sourceSettingsContent,
+		sourceSettingsErrors,
+		{
+			allowTrailingComma: true,
+		}
+	);
+	if (sourceSettingsErrors.length > 0) {
+		log.appendLine("Failed to parse source settings. Please make sure it contains correct JSON content.");
+		throw new Error(
+			"Failed to parse source settings. Please make sure it contains correct JSON content."
+		);
+	}
+
+	// get destination settings
+	const destinationSettingsContent = await readFile(destinationFile, {
+		encoding: "utf8",
+	});
+	const destinationSettingsErrors: ParseError[] = [];
+	let destinationSettings = parse(
+		destinationSettingsContent,
+		destinationSettingsErrors,
+		{
+			allowTrailingComma: true,
+		}
+	);
+	if (destinationSettingsErrors.length > 0) {
+		log.appendLine("Failed to parse destination settings. Please make sure it contains correct JSON content.");
+		throw new Error(
+			"Failed to parse settings.json. Please make sure it contains correct JSON content."
+		);
+	}
+
+	// merge settings
+	const isWorkspaceFile = destinationFile.endsWith(".code-workspace");
+	const mergedSettings = Object.assign(
+		{},
+		isWorkspaceFile ? destinationSettings.settings : destinationSettings,
+		sourceSettings
+	);
+
+	await writeFile(
+		destinationFile,
+		JSON.stringify(mergedSettings, null, 2),
+		{ encoding: "utf8" }
+	);
+
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand(COMMAND_NAME, async () => {
+		let log = vscode.window.createOutputChannel("MergeSettings");
+
+		let areSettingsSynced = false;
+
+		const workspaceFolderSettingsFile: string = vscode.workspace
+			.getConfiguration("mergeSettings")
+			.get("workspaceFolderSettingsFile") || "";
+
+		const useWorkspaceFolders = vscode.workspace.getConfiguration("mergeSettings").get("useWorkspaceFolders");
+
+		const sourceSettingsFiles: string = vscode.workspace.getConfiguration("mergeSettings").get("sourceSettingsFiles") || "";
+
+		const workspaceFile = vscode.workspace.workspaceFile;
+		if (!workspaceFile) {
+			log.appendLine("No workspace file found, so we do not known where to write the merged settings.");
+			return;
+		}
+
+
+		if (useWorkspaceFolders) {
+			if (!vscode.workspace.workspaceFolders) {
+				log.appendLine("No workspace folders found, so we do not know where to read the merged settings.");
+			} else {
+				vscode.workspace.workspaceFolders.map(async (folder) => {
+					const defaultSettingsFileLocation = path.resolve(
+						folder.uri.fsPath,
+						workspaceFolderSettingsFile
+					);
+					const defaultSettingsContent = await readFile(
+						defaultSettingsFileLocation,
+						{
+							encoding: "utf8",
+						}
+					);
+
+					mergeSettings(defaultSettingsContent, workspaceFile.fsPath);
+					log.appendLine(`Merged settings from ${defaultSettingsFileLocation}`);
+					areSettingsSynced = true;
+				});
+
+			}
+			sourceSettingsFiles.split('\n').map(async (sourceSettingsFile) => {
+				let sourceSettingsContent: string;
+
+				if (sourceSettingsFile.startsWith("http://") || sourceSettingsFile.startsWith("https://")) {
+					sourceSettingsContent = await fetch(sourceSettingsFile).then(res => res.text());
+
+				} else {
+					sourceSettingsContent = await readFile(sourceSettingsFile, {
+						encoding: "utf8",
+					});
+
+				}
+
+				mergeSettings(sourceSettingsContent, workspaceFile.fsPath);
+				log.appendLine(`Merged settings from ${sourceSettingsFile}`);
+				areSettingsSynced = true;
+			});
+		}
+
+		if (areSettingsSynced) {
+			vscode.window.showInformationMessage("Workspace Settings Synchronized");
+		} else {
+			vscode.window.showInformationMessage("No sources found, so settings are merged");
+		}
+	});
+
+	context.subscriptions.push(disposable);
+	if (
+		vscode.workspace
+			.getConfiguration("workspace-settings")
+			.get("runOnActivation")
+	) {
+		vscode.commands.executeCommand(COMMAND_NAME);
+	}
+}
+
+// this method is called when your extension is deactivated
